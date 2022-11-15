6b3b9162edc7d91ad7833888a5f9b0d43125db71
Basil-Kiselev <basiliento@gmail.com>
2022-11-15 13:29:59 +0700

    Добавлен валидотор для формы

--- DataSource/orbs.php
+++ DataSource/orbs.php
@@ -1,32 +1,39 @@
 <?php
-$orbs = [
-    [
-        'id' => 1,
-        'code' => 'alchemy',
-        'name' => 'Orb of Alchemy',
-    ],
-    [
-        'id' => 2,
-        'code' => 'blessed',
-        'name' => 'Blessed Orb',
-    ],
-    [
-        'id' => 3,
-        'code' => 'regal',
-        'name' => 'Regal Orb',
-    ],
-    [
-        'id' => 4,
-        'code' => 'divine',
-        'name' => 'Divine Orb',
-    ],
-    [
-        'id' => 5,
-        'code' => 'exalted',
-        'name' => 'Exalted Orb',
-    ],
-];
+// $orbs = [
+//     [
+//         'id' => 1,
+//         'code' => 'alchemy',
+//         'name' => 'Orb of Alchemy',
+//     ],
+//     [
+//         'id' => 2,
+//         'code' => 'blessed',
+//         'name' => 'Blessed Orb',
+//     ],
+//     [
+//         'id' => 3,
+//         'code' => 'regal',
+//         'name' => 'Regal Orb',
+//     ],
+//     [
+//         'id' => 4,
+//         'code' => 'divine',
+//         'name' => 'Divine Orb',
+//     ],
+//     [
+//         'id' => 5,
+//         'code' => 'exalted',
+//         'name' => 'Exalted Orb',
+//     ],
+// ];
 
-function getOrbs($orbs){
+
+function getOrbs(){
+    $fileName = 'DataSource\fileSource.txt';
+    $getData = file_get_contents($fileName);
+    $data = json_decode($getData,true);    
+    $orbs = $data['orbs']['items'];      
     return $orbs;
-}
+    }
+ 
+
